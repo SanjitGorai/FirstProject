30c789e63f7b22b6c4fdfdb4b6617d31d75c463f
Sanjit Gorai <sanjithprasad@handletechlabs.com>
2025-12-09 16:29:21 +0530

    first commit

---
+++ src/spg/A.java
@@ -0,0 +1,19 @@
+package spg;
+
+public class A {
+
+	public static void main(String[] args) {
+		
+		
+		String b="Niharika";
+		String c="Niharika";
+		
+	String s=new String("Niharika");
+	String s1=new String("Niharika");
+	
+	
+	System.out.println(b==c);
+
+	}
+
+}
