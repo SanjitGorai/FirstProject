7f7345f9c7fef79f3ab81557046be919dd6789ca
sanjay gorai <spgorai@gmail.com>
2026-01-20 20:48:14 +0530

    added class C

---
+++ src/spg/C.java
@@ -0,0 +1,19 @@
+package spg;
+
+public class C {
+
+    public static void main(String[] args) {
+
+
+        String b="Sanjit";
+        String c="Sanjit";
+
+//        String s=new String("Niharika");
+//        String s1=new String("Niharika");
+
+
+        System.out.println(b==c);
+
+    }
+
+}
