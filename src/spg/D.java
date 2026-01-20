fc6e7fad6e5b91089e388aa14e5a5090f603c3d0
sanjay gorai <spgorai@gmail.com>
2026-01-20 22:07:20 +0530

    add file D

---
+++ src/spg/D.java
@@ -0,0 +1,19 @@
+package spg;
+
+public class D {
+
+    public static void main(String[] args) {
+
+
+        String b="sanajy";
+        String c="Sanjit";
+
+//        String s=new String("Niharika");
+//        String s1=new String("Niharika");
+
+
+        System.out.println(b==c);
+
+    }
+
+}
